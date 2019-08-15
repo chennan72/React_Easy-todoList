be50ced517d5f33088fad2f1115df6126335d42e
chennan72 <chen.nan1@husky.neu.edu>
2019-08-15 20:19:30 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ListItem extends Component {
+    deleteTask(name) {
+        this.props.deleteItem(name)
+    }
+
+    completeTask(name) {
+        this.props.completeTask(name)
+    }
+
+    render() {
+        return (
+            <ul>
+                {
+                    this.props.data.map(element => {
+                        return (
+                            <li className="listItem" key={element.name}>
+                                <input type="checkbox"
+                                       checked={element.status === 1}
+                                       onChange={this.completeTask.bind(this, element.name)}/>
+                                <span
+                                    style={{textDecorationLine: element.status === 0 ? 'none' : 'line-through'}}>{element.name}</span>
+                                <button className="delete" onClick={this.deleteTask.bind(this, element.name)}>
+                                    Delete
+                                </button>
+                            </li>)
+                    })
+                }
+            </ul>
+        )
+    }
 }
 
-export default App;
+export default ListItem
